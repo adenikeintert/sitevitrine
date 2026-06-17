689aa300257658754cbd583a8acd1c1d760626ac
adenikeintert <adenikeintert@gmail.com>
2026-06-17 14:51:44 +0100

    fix: correct JSON-LD syntax by replacing single '@' with double '@@' for schema properties

--- resources/views/partials/schema.blade.php
+++ resources/views/partials/schema.blade.php
@@ -1,14 +1,14 @@
 @push('schema')
 <script type="application/ld+json">
 {
-    "@context": "https://schema.org",
-    "@type": "Organization",
+    "@@context": "https://schema.org",
+    "@@type": "Organization",
     "name": "{{ $infos['nom_societe'] ?? 'ADENIKE-INTER SARL' }}",
     "url": "{{ url('/') }}",
     "logo": "{{ $infos['logo'] ?? '' }}",
     "telephone": "{{ $infos['telephone'] ?? '' }}",
     "address": {
-        "@type": "PostalAddress",
+        "@@type": "PostalAddress",
         "streetAddress": "{{ $infos['adresse'] ?? '' }}",
         "addressCountry": "BJ"
     }
